99b97e2df66827c0c1a4189afa6485838feda09d
aliamk <alia.azarian@gmail.com>
2020-07-27 16:42:00 +0100

    created functionality for the game itself, random generation of questions with right and wrong answers, randomdisplay order of the questions, page change from the countdown to the game page,added more dyanmic html/css styling to the questions layout

--- script.js
+++ script.js
@@ -32,57 +32,90 @@ const wrongFormat = []
 
 // Scroll
 
+// Displays Game Page
+function showGamePage() {
+  gamePage.hidden = false
+  countdownPage.hidden = true
+}
+
+// Get random number up to a max number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  const correctEquations = getRandomInt(questionAmount) // for the getRandomInt(max)
+  console.log('correct equations', correctEquations)
   // Set amount of wrong equations
-  // const wrongEquations = 
+  const wrongEquations = questionAmount - correctEquations
+  console.log('wrong equations', wrongEquations)
   // Loop through, multiply random numbers up to 9, push to array
-  // for (let i = 0 i < correctEquations i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber
-  //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`
-  //   equationObject = { value: equation, evaluated: 'true' }
-  //   equationsArray.push(equationObject)
-  // }
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = getRandomInt(9)
+    secondNumber = getRandomInt(9)
+    const equationValue = firstNumber * secondNumber
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`
+    equationObject = { value: equation, evaluated: 'true' }
+    equationsArray.push(equationObject)
+  }
   // Loop through, mess with the equation results, push to array
-  // for (let i = 0 i < wrongEquations i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber
-  //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`
-  //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`
-  //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`
-  //   const formatChoice = 
-  //   const equation = wrongFormat[formatChoice]
-  //   equationObject = { value: equation, evaluated: 'false' }
-  //   equationsArray.push(equationObject)
-  // }
+  for (let i = 0; i < wrongEquations; i++) {
+    firstNumber = getRandomInt(9)
+    secondNumber = getRandomInt(9)
+    const equationValue = firstNumber * secondNumber
+    wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`
+    wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`
+    const formatChoice = getRandomInt(3)
+    const equation = wrongFormat[formatChoice]
+    equationObject = { value: equation, evaluated: 'false' }
+    equationsArray.push(equationObject)
+  }
+  shuffle(equationsArray) // take the generated questions and shuffle them so that all the true questions aren't generated together
+  // console.log('equations array:', equationsArray)
+  // equationsToDom()
+}
+
+// Add questions to DOM
+function equationsToDom() {
+  equationsArray.forEach((equation) => {
+    // Item
+    const item = document.createElement('div')
+    item.classList.add('item')
+    // Equation Text
+    const equationText = document.createElement('h1')
+    equationText.textContent = equation.value
+    // Append
+    item.appendChild(equationText)
+    itemContainer.appendChild(item)
+  })
 }
 
 // Dynamically adding correct/incorrect equations
-// function populateGamePage() {
-//   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = ''
-//   // Spacer
-//   const topSpacer = document.createElement('div')
-//   topSpacer.classList.add('height-240')
-//   // Selected Item
-//   const selectedItem = document.createElement('div')
-//   selectedItem.classList.add('selected-item')
-//   // Append
-//   itemContainer.append(topSpacer, selectedItem)
+function populateGamePage() {
+  // Reset DOM, Set Blank Space Above
+  itemContainer.textContent = ''
+  // Spacer
+  const topSpacer = document.createElement('div')
+  topSpacer.classList.add('height-240')
+  // Selected Item
+  const selectedItem = document.createElement('div')
+  selectedItem.classList.add('selected-item')
+  // Append
+  itemContainer.append(topSpacer, selectedItem)
 
-//   // Create Equations, Build Elements in DOM
+  // Create Equations, Build Elements in DOM
+  createEquations()
+  equationsToDom()
+  // Set Blank Space Below
+  const bottomSpacer = document.createElement('div')
+  bottomSpacer.classList.add('height-500')
+  itemContainer.appendChild(bottomSpacer)
+}
 
-//   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div')
-//   bottomSpacer.classList.add('height-500')
-//   itemContainer.appendChild(bottomSpacer)
-// }
-
+// Display 3, 2, 1, GO!
 function countdownStart() {
   countdown.textContent = '3'
   setTimeout(() => {
@@ -101,6 +134,8 @@ function showCountdown() {
   countdownPage.hidden = false
   splashPage.hidden = true
   countdownStart()
+  populateGamePage() 
+  setTimeout(showGamePage, 400)
 }
 
 // When User selects a game - get the game value from the selected radio button
